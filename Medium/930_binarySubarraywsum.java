ea694db74d035c39bd0c1577c48336f458bec238
Aman Anand <142965409+amananand141@users.noreply.github.com>
2026-03-04 03:02:38 +0530

    Create 930_binarySubarraywsum.java

---
+++ Medium/930_binarySubarraywsum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        map.put(0,1);
+
+        int prefix = 0;
+
+        for(int num : nums){
+            prefix += num;
+
+            if(map.containsKey(prefix-goal)){
+                count += map.get(prefix-goal);
+            }
+
+            map.put(prefix, map.getOrDefault(prefix, 0)+1);
+        }
+
+        return count;
+    }
+}
