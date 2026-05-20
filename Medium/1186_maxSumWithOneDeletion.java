9117fe6d47bb34fa738516ea7444acae454f4eed
Aman Anand <142965409+amananand141@users.noreply.github.com>
2026-05-20 21:19:09 +0530

    Update 1186_maxSumWithOneDeletion.java

--- Medium/1186_maxSumWithOneDeletion.java
+++ Medium/1186_maxSumWithOneDeletion.java
@@ -1,37 +1,22 @@
 class Solution {
     public int maximumSum(int[] arr) {
-        int n = arr.length; 
-        if(n == 1)return arr[0];
+        int oneDelete = Integer.MIN_VALUE;
+        int noDelete = arr[0];
+        int result = arr[0];
 
-        int[] dp1 = new int[n];
-        int[] dp2 = new int[n];
-        
-        int max = arr[0];
-        dp1[0] = arr[0];
-        dp2[n-1] = arr[n-1];
-
-        for(int i=1; i<n; i++){
-            if(dp1[i-1] > 0){
-                dp1[i] = dp1[i-1]+arr[i];
+        for(int i=1; i<arr.length; i++){
+            int prevNo = noDelete;
+            int prevOne = oneDelete;
+            noDelete = Math.max(noDelete+arr[i], arr[i]);
+            int v2;
+            if(prevOne == Integer.MIN_VALUE){
+                v2 = arr[i];
             }else{
-                dp1[i] = arr[i];
+                v2 = prevOne+arr[i];
             }
-            max = Math.max(max, dp1[i]);
+            oneDelete = Math.max(v2, prevNo);
+            result = Math.max(result, Math.max(noDelete, oneDelete));
         }
-
-        for(int i=n-2; i>=0; i--){
-            if(dp2[i+1] > 0){
-                dp2[i] = dp2[i+1]+arr[i];
-            }else{
-                dp2[i] = arr[i];
-            }
-        }
-
-        for(int i=1; i<n-1; i++){
-            if(arr[i] < 0){
-                max = Math.max(max, dp1[i-1]+dp2[i+1]);
-            }
-        }
-        return max;
+        return result;
     }
 }
